a45713af72eacc0908ee4c66bfd7066af0d9e895
yvshi <1714343401@qq.com>
2021-07-11 09:06:07 +0800

    local env

--- backend/config.py
+++ backend/config.py
@@ -15,7 +15,7 @@ REMEMBER_COOKIE_REFRESH_EACH_REQUEST = True # 自动刷新时长
 # 数据库配置
 MONGODB_SETTINGS = {
     "db": 'cloud_disk',
-    'host': '192.168.10.5',
+    'host': '192.168.2.100',
     'port': 27017,
     'connect': False,
     'username': 'root',
@@ -25,7 +25,7 @@ MONGODB_SETTINGS = {
 
 # HDFS 设置
 HADOOP_MASTER = [
-    "192.168.10.5:50070",
+    "192.168.2.100:50070",
 ]
 HADOOP_USER_NAME='root'
 HDFS_HOME = '/cloud'  # hdfs中的根目录
